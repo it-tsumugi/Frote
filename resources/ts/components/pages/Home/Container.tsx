261506f0e5ff88742fb8986bbced5214c1315c1b
it-tsumugi <tsumugi.it@gmail.com>
2022-08-12 20:09:54 +0900

    d

--- resources/ts/components/pages/Home/Container.tsx
+++ resources/ts/components/pages/Home/Container.tsx
@@ -2,11 +2,9 @@ import { VFC } from 'react'
 import { taskListsState } from '../../../state/atom'
 import { useRecoilValue } from 'recoil'
 import { PHome } from './Presenter'
-import { fetchAllTaskLists } from '../../../api/fetchAllTaskLists'
 
 export const Home: VFC = () => {
   const taskLists = useRecoilValue(taskListsState)
 
-  // fetchAllTaskLists({})
   return <PHome taskLists={taskLists} />
 }
