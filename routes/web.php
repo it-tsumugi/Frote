8dce9f014ba2f4a6af4e3dba9cb6ce10627792d4
it-tsumugi <tsumugi.it@gmail.com>
2022-09-05 15:51:03 +0900

    web.phpの変更

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@ use Illuminate\Support\Facades\Route;
 
 // Auth::routes();
 
+if (App::environment('production')) {
+    URL::forceScheme('https');
+}
+
 Route::get('/{any}', function () {
     return view('welcome');
 })->where('any','.*');;
